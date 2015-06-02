08f9c146a4633b83c4fb2f6f407534f3c41876bd
Lucas Weber <weberl48@outlook.com>
2015-06-02 16:34:08 -0600

    urls added

--- js/index.js
+++ js/index.js
@@ -1,18 +1,24 @@
 var h1 = document.createElement('h1');
 h1.innerHTML = "OMBD Movies About Pizza";
 document.body.appendChild(h1);
-
+// sending the request
 var xhr = new XMLHttpRequest();
 xhr.open('get', "http://www.omdbapi.com/?s=pizza");
 xhr.addEventListener('load', function () {
+  //recieving the request
   var response = xhr.response;
+  //parsing the data into an object
   var responseData = JSON.parse(response);
-
-
+  //get all the titles
   for (var i = 0; i < responseData.Search.length; i++) {
+
+    var a = document.createElement('a');
+    a.innerHTML = responseData.Search[i].Title;
+    a.href = '/show.html?';
+
+
     var p = document.createElement('p');
-    responseData.Search[i];
-    p.innerHTML = responseData.Search[i].Title;
+    p.appendChild(a);
     document.body.appendChild(p);
 }
 

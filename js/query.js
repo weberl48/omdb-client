7227de01fb34810e54b6425fe868d043cd4873db
Lucas Weber <weberl48@outlook.com>
2015-06-03 15:51:51 -0600

    appended reponse data to movie.html

--- js/query.js
+++ js/query.js
@@ -1,5 +1,5 @@
-var omdbQuery = "?i=tt0095690&plot=short&r=json"
-// var omdbQuery = document.location.search;
+var omdbQuery = document.location.search;
+// var omdbQuery = "?i=tt0095690&plot=short&r=json"
 var objectNew = {}
 omdbQuery = omdbQuery.replace("?" , " ")
 omdbQuery = omdbQuery.split("&")
@@ -12,3 +12,20 @@ for (var i = 0; i < omdbQuery.length; i++) {
 }
 
 var omdbID = objectNew.i;
+
+var xhrID = new XMLHttpRequest();
+xhrID.open('GET', "http://www.omdbapi.com/?i=" + omdbID, true);
+xhrID.addEventListener('load', function () {
+  //recieving the request
+  var response = xhrID.response;
+  //parsing the data into an object
+  var responseData = JSON.parse(response);
+  var a = document.createElement('a');
+  a.innerHTML = responseData
+  var p = document.createElement('p');
+  p.appendChild(a);
+
+document.body.appendChild(p);
+
+ });
+xhrID.send();
